1b22d3c1d995ed23774fb5610618a6a508136a5a
clamee0229 <chris.lamee96@gmail.com>
2021-09-17 14:50:27 -0400

    initial commit

---
+++ loopChallenges.js
@@ -0,0 +1,32 @@
+// Print odds 1-20
+for(var i = 0; i<21; i++){
+    if (i % 2 == 1){
+        console.log(i);
+    }
+}
+
+// Decreasing Multiples of 3
+for(var i = 100; i>0; i--){
+    if (i % 3 == 0){
+        console.log(i);
+    }
+}
+
+// Print the sequence
+for(var i = 4; i>-4; i = i-1.5){
+    console.log(i)
+}
+
+// Sigma
+var sum = 0;
+for(var i = 0; i<101; i++){
+    sum+=i;
+}
+console.log(sum);
+
+// Factorial
+var product = 1;
+for(var i = 1; i<13; i++){
+    product*=i;
+}
+console.log(product);
